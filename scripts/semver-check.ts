980e7f1a177787636b0e57add473c4bacae2385f
Mathis Pinsault <pinsaultm@gmail.com>
2023-11-10 00:32:49 +0100

    Add sort check for package.json

--- scripts/semver-check.ts
+++ scripts/semver-check.ts
@@ -2,30 +2,39 @@ import { gt, valid } from "semver";
 
 import pkg from "../package.json";
 
-const infos = Bun.spawnSync({ cmd: ["pnpm", "info", "--json"] });
-if (!infos.success) {
-  console.info("Bypassing semver check");
-  console.error("Could not get package info, got:\n%s", infos.stderr);
+try {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/maastrich/moonx/main/package.json",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const infos = await response.json();
+
+  const current = pkg.version;
+  const previous = infos.version;
+
+  if (!previous || !valid(previous) || !valid(current)) {
+    console.error(
+      "Could not compare versions, got: previous=%s, current=%s",
+      previous,
+      current,
+    );
+    process.exit(1);
+  }
+
+  if (!gt(current, previous)) {
+    console.error(
+      "Current version %s is not greater than previous version %s",
+      current,
+      previous,
+    );
+    process.exit(1);
+  }
+} catch (error) {
+  console.error("Could not resolve previous version:\n\t%s", error);
   process.exit(0);
-}
-
-const current = pkg.version;
-const previous = JSON.parse(infos.stdout.toString()).version;
-
-if (!previous || !valid(previous) || !valid(current)) {
-  console.error(
-    "Could not compare versions, got: previous=%s, current=%s",
-    previous,
-    current,
-  );
-  process.exit(1);
-}
-
-if (!gt(current, previous)) {
-  console.error(
-    "Current version %s is not greater than previous version %s",
-    current,
-    previous,
-  );
-  process.exit(1);
 }
