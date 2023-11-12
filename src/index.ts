641fba0a376c0090495caba9c596c7d43771209d
Mathis Pinsault <pinsaultm@gmail.com>
2023-11-12 03:25:47 +0100

    Fix for-of loop in CLI commands iteration

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ cli
     stdout.end();
   });
 
-for (const name in commands.keys()) {
+for (const name of commands.keys()) {
   cli
     .command(`${name} [...workspaces]`, "", { allowUnknownOptions: true })
     .action(async (wss: Array<string>, options) => {
