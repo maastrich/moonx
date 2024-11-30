4bc57455841e67c1d1102847db5db5a655444e99
maastrich <pinsaultm@gmail.com>
2024-11-30 15:23:24 +0100

    change: update workflow job name and refactor setup-binary script

--- scripts/setup-binary.js
+++ scripts/setup-binary.js
@@ -1,9 +1,9 @@
+import { renameSync } from "node:fs";
+import { join } from "node:path";
+
 const { platform, arch } = process;
 
-// move bin/$platform-$arch to bin/moonx
-const fs = require("node:fs");
-const path = require("node:path");
-const src = path.join(process.cwd(), "bin", `${platform}-${arch}`);
-const dest = path.join(process.cwd(), "bin", "moonx");
+const src = join(process.cwd(), "bin", `${platform}-${arch}`);
+const dest = join(process.cwd(), "bin", "moonx");
 
-fs.renameSync(src, dest);
+renameSync(src, dest);
